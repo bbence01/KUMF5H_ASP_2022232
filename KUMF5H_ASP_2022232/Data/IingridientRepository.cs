b2e0c9b1770d23449d62b7a1c2c24be712519b89
Bognár Bence <bbence210@gmail.com>
2023-05-11 11:19:18 +0200

    IngridientRepository

---
+++ KUMF5H_ASP_2022232/Data/IingridientRepository.cs
@@ -0,0 +1,15 @@
+﻿using KUMF5H_ASP_2022232.Models;
+
+namespace KUMF5H_ASP_2022232.Data
+{
+    public interface IingridientRepository
+    {
+
+        void Create(Ingredient ingredient);
+        void Delete(Ingredient ingredient);
+        void Delete(string id);
+        IEnumerable<Ingredient> GetAll();
+        Ingredient GetOne(string id);
+        List<Ingredient> GetIngredientsForRequest(string id);
+    }
+}
