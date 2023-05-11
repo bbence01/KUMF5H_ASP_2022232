4204b6ea456808b84273e65f79ebb3ae5c29a2f8
Bognár Bence <bbence210@gmail.com>
2023-05-11 13:36:14 +0200

    FoodrequestController / FoodrequestViewModel
    GetAll, Deatails

--- KUMF5H_ASP_2022232/Controllers/FoodrequestController.cs
+++ KUMF5H_ASP_2022232/Controllers/FoodrequestController.cs
@@ -1,83 +1,55 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using KUMF5H_ASP_2022232.Data;
+using KUMF5H_ASP_2022232.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KUMF5H_ASP_2022232.Controllers
 {
     public class FoodrequestController : Controller
     {
-        // GET: FoodrequestController
-        public ActionResult Index()
+        private readonly IFoodRequestRepository repository;
+        private readonly UserManager<FoodUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public FoodrequestController(IFoodRequestRepository repository, UserManager<FoodUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.repository = repository;
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(this.repository.GetAll().OrderBy(f => f.IsDone));
+        }
+
+        public IActionResult Details(string id)
+        {
+            var food = this.repository.GetOne(id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            return View(food);
+        }
+
+        [Authorize]
+        public IActionResult Create()
         {
             return View();
         }
 
-        // GET: FoodrequestController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
 
-        // GET: FoodrequestController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+      
 
-        // POST: FoodrequestController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+    
 
-        // GET: FoodrequestController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
 
-        // POST: FoodrequestController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+   
 
-        // GET: FoodrequestController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: FoodrequestController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+ 
     }
 }
---
+++ KUMF5H_ASP_2022232/Models/FoodRequestViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace KUMF5H_ASP_2022232.Models
+{
+    public class FoodRequestViewModel
+    {
+        [StringLength(100)]
+        public string Name { get; set; }
+
+
+        [StringLength(1000)]
+        [MinLength(5)]
+        public string Description { get; set; }
+
+
+
+        [DefaultValue(false)]
+        public bool IsDone { get; set; }
+
+    }
+}
