f99a4f4b680e74a602ca4e7432f346812c42e847
Bognár Bence <bbence210@gmail.com>
2023-05-11 11:17:35 +0200

    FoodUserRepository

---
+++ KUMF5H_ASP_2022232/Data/IFoodUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using KUMF5H_ASP_2022232.Models;
+
+namespace KUMF5H_ASP_2022232.Data
+{
+    public interface IFoodUserRepository
+    {
+        FoodUser CreateFooduser(FoodUser foodUser);
+        void DeleteFooduser(string foodUserid);
+        IEnumerable<FoodUser> GetAllFoodusers();
+        FoodUser GetFooduserById(string foodUserid);
+        FoodUser UpdateFooduser(FoodUser foodUser);
+        void AddRange(List<FoodUser> foodUsers);
+    }
+}
