915fd0dfa7801dd1b600f1a3a0321187f2b1f99f
Bognár Bence <bbence210@gmail.com>
2023-05-11 10:44:17 +0200

    Add project files.

---
+++ KUMF5H_ASP_2022232/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using KUMF5H_ASP_2022232.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KUMF5H_ASP_2022232.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ KUMF5H_ASP_2022232/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KUMF5H_ASP_2022232.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
